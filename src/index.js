1c480fae7a6895eddd2e47cef6880ab654d9ea90
DBertazioli <d.bertazioli@campus.unimib.it>
2019-02-28 22:12:17 +0100

    .

--- src/index.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('*[data-href]').forEach((tag) => {
-    tag.addEventListener('click', (e) => {
-        console.log('click');
-        document.querySelector(tag.dataset.href).scrollIntoView({
-            behavior: 'smooth',
-        });
-    });
-});
